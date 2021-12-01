bd7a0d83d2cbb51a0628319a5a8c5ab5994a4344
jorgetovar <jorgetovar621@gmail.com>
2021-12-01 18:02:24 -0500

    Initial configuration database

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,4 +1,15 @@
-CREATE TABLE IF NOT EXISTS messages (
-    id                     VARCHAR(60)  DEFAULT RANDOM_UUID() PRIMARY KEY,
-    text                   VARCHAR      NOT NULL
-    );
+CREATE TABLE IF NOT EXISTS steps
+(
+    id            INT DEFAULT RANDOM() PRIMARY KEY,
+    resource_link VARCHAR NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS roadmaps
+(
+    id      VARCHAR(60) DEFAULT RANDOM_UUID() PRIMARY KEY,
+    mentor  VARCHAR NOT NULL,
+    step_id INT,
+
+    CONSTRAINT fk_steps FOREIGN KEY (step_id) REFERENCES steps (id)
+
+);
